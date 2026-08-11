77a2d2e172b40a48706cb46680e69d2a4062a5e2
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2026-08-11 20:28:43 +0100

    install zig LSP

--- init.lua
+++ init.lua
@@ -702,6 +702,7 @@ do
     -- gopls = {},
     pyright = {},
     -- rust_analyzer = {},
+    zls = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
