f0d6095c4130b78b2aaa07f8d32a1acb700b6561
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2026-08-11 20:38:05 +0100

    autoformat python files on save

--- init.lua
+++ init.lua
@@ -790,7 +790,7 @@ do
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
         -- lua = true,
-        -- python = true,
+        python = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
@@ -805,7 +805,7 @@ do
     formatters_by_ft = {
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
